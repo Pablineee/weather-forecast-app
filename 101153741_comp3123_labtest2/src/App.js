02b75fd6d73f57b6e0558d4d6abce16edae87870
System Administrator <110475574+Pablineee@users.noreply.github.com>
2024-11-28 18:02:54 -0500

    Reformatted UI design, more required.

--- 101153741_comp3123_labtest2/src/App.js
+++ 101153741_comp3123_labtest2/src/App.js
@@ -40,22 +40,47 @@ function App() {
     <div className="container mt-5">
       <SearchBar onSearch={fetchWeather} />
 
-      {/* Day Forecast Visual Display */}
-      <div>
+      <div className="row mt-4 mb-5 w-100">
+        {/* Left Column */}
+        <div className="col-md-4 d-flex">
+          <div className="card text-left p-4 w-100">
+            <p className="card-text">Thursday</p>
+            <p className="card-text">{() => {
+              
+            }}</p>
+            <p className="card-text">Canada</p>
+            <img className='w-50' src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+            <p className="card-text">{Math.round(weather.main.temp)}°C</p>
+            <p className="card-text">{weather.weather[0].main}</p>
+          </div>
+        </div>
 
-      </div>
+        {/* Right Column */}
+        <div className="col-md-8 d-flex flex-column">
+          {/* Current Weather */}
+          <div className="card flex-fill p-4 mb-3">
+            <h1 className="card-title mb-5">{weather.name}</h1>
+            <p className="card-text"><strong><span className='float-start'>Temp</span></strong><span className='float-end'>{weather.main.temp}°C</span></p>
+            <p className="card-text"><strong><span className='float-start'>Humidity</span></strong><span className='float-end'>{weather.main.humidity}%</span></p>
+            <p className="card-text"><strong><span className='float-start'>Air Pressure</span></strong><span className='float-end'>{weather.main.pressure} mb</span></p>
+            <p className="card-text"><strong><span className='float-start'>Max Temp</span></strong><span className='float-end'>{weather.main.temp_max}°C</span></p>
+            <p className="card-text"><strong><span className='float-start'>Min Temp</span></strong><span className='float-end'>{weather.main.temp_min}°C</span></p>
+          </div>
 
-      <div className='card ps-5 pb-5 pt-4'>
-        <h1 className='mt-4 mb-5'>{weather.name}</h1>
-        <p className='WeatherDataItem'><strong>Temp </strong><span className='float-end me-5'>{weather.main.temp}°C</span></p>
-        <p className='WeatherDataItem'><strong>Humidity </strong><span className='float-end me-5'>{weather.main.humidity}%</span></p>
-        <p className='WeatherDataItem'><strong>Air Pressure </strong><span className='float-end me-5'>{weather.main.pressure} mb</span></p>
-        <p className='WeatherDataItem'><strong>Max Temp </strong><span className='float-end me-5'>{weather.main.temp_max}°C</span></p>
-        <p className='WeatherDataItem'><strong>Min Temp </strong><span className='float-end me-5'>{weather.main.temp_min}°C</span></p>
-      </div>
-
-      <div>{/* Weekly Forecast */}
-
+          {/* Weekly Forecast */}
+          <div className="card p-4">
+            <p>
+              <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+            </p>
+            <p>
+              <span>Mon</span>
+              <span>Tue</span>
+              <span>Wed</span>
+              <span>Thu</span>
+              <span>Fri</span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
     
